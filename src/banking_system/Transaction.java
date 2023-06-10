232dd8fb538e24c3bbf94aba5c5c1df477305a00
Aryaman <aryaman1110@gmail.com>
2023-06-11 00:57:56 +0530

    Transaction file commit

--- src/banking_system/Transaction.java
+++ src/banking_system/Transaction.java
@@ -8,10 +8,30 @@ package banking_system;
  *
  * @author aryam
  */
-public class Transaction {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Transaction  extends JFrame{
+    
+    Transaction(){
+       setSize(950,900);
+        setLocation(300,0); 
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2=i1.getImage().getScaledInstance(900,900,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(0,0,900,900);
+        add(image);
+        setVisible(true);
+        setLayout(null);
+        
+    
+    }
     
     public static void main(String args[]){
-        
+        new Transaction();
     }
     
 }
